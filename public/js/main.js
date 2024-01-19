b0acd33cf925330c24042f77fe9076470790e26a
Soulayy <s.sultaway@gmail.com>
2024-01-19 14:23:37 +0100

    end project

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,9 @@
 let counts = setInterval(updated);
 let upto = 0;
 function updated() {
-        let count = document.getElementById("counter");
-        count.innerHTML = (upto += 100);
-        if (upto === 35400) {
+        let count = document.getElementById("counter1");
+        count.innerHTML = (upto += 30000);
+        if (upto === 4500000) {
         clearInterval(counts);
     }
 }
@@ -11,9 +11,9 @@ function updated() {
 let counts1 = setInterval(updated1);
 let upto1 = 0;
 function updated1() {
-        let count1 = document.getElementById("counter1");
-        count1.innerHTML = (upto1 += 100);
-        if (upto1 === 5000) {
+        let count1 = document.getElementById("counter2");
+        count1.innerHTML = (upto1 += 20000);
+        if (upto1 === 3000000) {
         clearInterval(counts1);
     }
 }
@@ -21,9 +21,9 @@ function updated1() {
 let counts2 = setInterval(updated2);
 let upto2 = 0;
 function updated2() {
-        let count2 = document.getElementById("counter2");
-        count2.innerHTML = (upto2 += 10);
-        if (upto2 === 470) {
+        let count2 = document.getElementById("counter3");
+        count2.innerHTML = (upto2 += 10000);
+        if (upto2 === 2000000) {
         clearInterval(counts2);
     }
 }
